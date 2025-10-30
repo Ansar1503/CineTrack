9bd12a9d5b48694e8207d71488334832e68f4108
Ansar1503 <muhammedansarma1503@gmail.com>
2025-10-30 21:49:45 +0530

    Add error handling for user authentication in middleware

--- Backend/src/constants/ResponseMessages.ts
+++ Backend/src/constants/ResponseMessages.ts
@@ -2,6 +2,8 @@ export const ERROR_MESSAGES = {
   JWT_ACCESS_SECRET_NOT_FOUND: "Invalid Jwt Access Secret",
   JWT_REFRESH_SECRET_NOT_FOUND: "Invalid Jwt Refresh Secret",
   JWT_EMAIL_SECRET_NOT_FOUND: "Invalid Jwt Email Secret",
+  USER_NOT_FOUND: "No user found with the userId",
+  NO_TOKEN_PAYLOAD: "No Token Payload Found",
   UNAUTHORIZED: "Unauthorised",
   INVALID_TOKEN: "Invalid Token",
 };
--- Backend/src/middlewares/auth.middleware.ts
+++ Backend/src/middlewares/auth.middleware.ts
@@ -2,12 +2,13 @@ import type { Request, Response, NextFunction } from "express";
 import { ERROR_MESSAGES } from "../constants/ResponseMessages";
 import { STATUSCODES } from "../constants/StatusCodes";
 import { verifyAccessToken } from "../config/jwt";
+import { prisma } from "@src/prisma/client";
 
 export interface AuthRequest extends Request {
   userId?: string;
 }
 
-export const authMiddleware = (
+export const authMiddleware = async (
   req: AuthRequest,
   res: Response,
   next: NextFunction
@@ -21,7 +22,19 @@ export const authMiddleware = (
         .json({ message: ERROR_MESSAGES.UNAUTHORIZED });
 
     const decoded = verifyAccessToken(token);
-
+    if (!decoded.userId) {
+      return res
+        .status(STATUSCODES.BAD_REQUEST)
+        .json({ message: ERROR_MESSAGES.NO_TOKEN_PAYLOAD });
+    }
+    const userExists = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+    });
+    if (!userExists) {
+      return res.status(STATUSCODES.BAD_REQUEST).json({
+        message: ERROR_MESSAGES.USER_NOT_FOUND,
+      });
+    }
     req.userId = decoded.userId;
     next();
   } catch (error) {
