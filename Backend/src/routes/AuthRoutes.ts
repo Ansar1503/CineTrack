b82750432051bb54220f2c5522bfbf89b784169f
Ansar1503 <muhammedansarma1503@gmail.com>
2025-10-30 22:44:11 +0530

    Add RegisterUser controller to user registration route

--- Backend/src/routes/AuthRoutes.ts
+++ Backend/src/routes/AuthRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { ROUTES } from "../constants/RouteConstants";
+import { RegisterUser } from "@src/controller/AuthController/RegisterUserController";
 
 const route = Router();
 
-route.post(ROUTES.AUTH.REGISTER);
+route.post(ROUTES.AUTH.REGISTER, RegisterUser);
 
 export default route;
