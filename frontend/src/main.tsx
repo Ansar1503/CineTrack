f156bcabee7360a5185baabb07097b9639bf1c2c
Ansar1503 <muhammedansarma1503@gmail.com>
2025-10-31 01:51:18 +0530

    feat: integrate Redux for state management and add authentication slice

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </PersistGate>
+  </Provider>
 );
